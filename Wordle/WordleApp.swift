798378752e91442032486cf5f2f19e654274537d
Maertens Yann Christophe <iroshinidako@gmail.com>
2022-04-03 11:45:06 +0200

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Maertens Yann-Christophe on 03/04/22.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
